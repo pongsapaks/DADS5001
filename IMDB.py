4ffa6294cc9e1684c944612623cce7aaebe752f8
pongsapak.s <117065239+pongsapaks@users.noreply.github.com>
2023-02-05 12:56:05 +0700

    filepy

---
+++ IMDB.py
@@ -0,0 +1,6 @@
+import sys
+import pandas as pd
+import numpy as np
+print( f"Python {sys.version}\nPandas {pd.__version__}\nNumPy {np.__version__}" ) 
+# Load the pokemon dataset
+df = pd.read_csv('https://github.com/pongsapaks/DADS5001/blob/d548aa3d68af17cdeff6a2928daf40218384eaff/Untitled.ipynb')  
